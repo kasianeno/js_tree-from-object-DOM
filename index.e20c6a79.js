dccd35aae69e09e0bd6e48c1c333de33d7c800f4
Oleksandr <dedikad@gmail.com>
2025-01-31 17:22:09 +0100

    make build

---
+++ index.e20c6a79.js
@@ -0,0 +1,2 @@
+!function e(n,r){var t=document.createElement("ul");for(var a in r)if(r.hasOwnProperty(a)){var l=document.createElement("li");l.textContent=a,"object"===r[a]&&null!==r[a]&&e(l,r[a]),t.appendChild(l)}n.appendChild(t)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.e20c6a79.js.map
